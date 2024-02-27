197320c88d74a4af6ee9facb9c960a46ab1ed1fd
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2024-02-27 07:28:55 -0300

    style: Creation of a skeleton for loading the table body

---
+++ src/components/table-body-skeleton.tsx
@@ -0,0 +1,27 @@
+import { Skeleton } from './ui/skeleton'
+import { TableCell, TableRow } from './ui/table'
+
+export function TableBodySkeleton() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+      <TableCell>
+        <Skeleton className="h-4 w-32" />
+      </TableCell>
+    </TableRow>
+  )
+}
